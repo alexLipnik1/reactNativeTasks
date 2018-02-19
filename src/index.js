57b677392ef71058839dba2444bdaec02e2fe5ba
alexLipnik1 <alex.lipnik111@gmail.com>
2018-02-19 14:27:52 +0200

    initial

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { AppRegistry } from 'react-native';
+import App from './App';
+
+AppRegistry.registerComponent('App', () => App );
+AppRegistry.runApplication('App', {
+  rootTag: document.getElementById('root')
+})
